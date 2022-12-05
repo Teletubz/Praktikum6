9a91ebb17cc61883cd80249917319b953291cce0
Gilang <gilangphb@gmail.com>
2022-12-05 16:02:45 +0700

    Menambahkan CPU.java

---
+++ CPU.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.relasiclass;
+
+/**
+ *
+ * @author hp
+ */
+public class CPU {
+      
+    //Dependensi
+    public String merk;
+    
+    public void on(){
+        System.out.println("CPU menyala......");
+    }
+    
+    public void off(){
+        System.out.println("CPU mati......");
+    }
+    
+}
