a2c187df1ea1ad38b462dd393adbdeb61c01f19f
Gilang <gilangphb@gmail.com>
2022-12-05 16:04:14 +0700

    Menambahkan Keyboard.java

---
+++ Keyboard.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.relasiclass;
+
+/**
+ *
+ * @author hp
+ */
+public class Keyboard {
+    
+    public String merk;
+    
+    public void ketik(){
+        System.out.println("Mengetik.....");
+    }
+    
+}
